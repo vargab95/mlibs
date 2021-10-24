a7f729837367c8d307cf9105710efd08f867f420
Balázs János Varga <vargab95@gmail.com>
2021-10-24 22:51:32 +0200

    Fix bug in m_map_iterator

--- m_map/src/m_map_iterator.c
+++ m_map/src/m_map_iterator.c
@@ -1,6 +1,8 @@
 #include "m_map.h"
 #include <m_libs/m_mem.h>
 
+static void find_and_set_next_table_root(m_map_iterator_t *iterator);
+
 m_map_iterator_t *m_map_iterator_create(m_map_t *map)
 {
     m_map_iterator_t *iterator = (m_map_iterator_t *)m_mem_malloc(sizeof(m_map_iterator_t));
@@ -31,13 +33,8 @@ void m_map_iterator_next(m_map_iterator_t *iterator)
         return;
     }
 
-    if (iterator->hash_table_idx >= (iterator->map->size - 1))
-    {
-        return;
-    }
-
     iterator->hash_table_idx++;
-    iterator->element = iterator->map->table[iterator->hash_table_idx].next;
+    find_and_set_next_table_root(iterator);
 }
 
 m_com_sized_data_t *m_map_iterator_key(const m_map_iterator_t *const iterator)
@@ -52,6 +49,21 @@ m_com_sized_data_t *m_map_iterator_value(const m_map_iterator_t *const iterator)
 
 void m_map_iterator_reset(m_map_iterator_t *iterator)
 {
-    iterator->element = iterator->map->table[0].next;
     iterator->hash_table_idx = 0;
+    find_and_set_next_table_root(iterator);
+}
+
+static void find_and_set_next_table_root(m_map_iterator_t *iterator)
+{
+    iterator->element = NULL;
+    while ((iterator->hash_table_idx < iterator->map->size) &&
+           (iterator->map->table[iterator->hash_table_idx].next == NULL))
+    {
+        iterator->hash_table_idx++;
+    }
+
+    if (iterator->hash_table_idx < iterator->map->size)
+    {
+        iterator->element = iterator->map->table[iterator->hash_table_idx].next;
+    }
 }
