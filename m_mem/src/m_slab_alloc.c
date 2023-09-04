6034c9b90c9d0586b81d434759d5e0b6a4ffe75b
Balázs János Varga <vargab95@gmail.com>
2023-09-04 09:39:09 +0200

    Rename id to context in slab allocator

--- m_mem/src/m_slab_alloc.c
+++ m_mem/src/m_slab_alloc.c
@@ -5,11 +5,11 @@
 #endif
 
 static m_context_id_t create_context(m_allocator_config_t config);
-static void destroy_context(m_context_id_t id);
-static m_com_sized_data_t *malloc_impl(m_context_id_t id, size_t size);
-static m_com_sized_data_t *calloc_impl(m_context_id_t id, uint32_t number, size_t size);
-static m_com_sized_data_t *realloc_impl(m_context_id_t id, m_com_sized_data_t *data, size_t size);
-static void free_impl(m_context_id_t id, m_com_sized_data_t *data);
+static void destroy_context(m_context_id_t context);
+static m_com_sized_data_t *malloc_impl(m_context_id_t context, size_t size);
+static m_com_sized_data_t *calloc_impl(m_context_id_t context, uint32_t number, size_t size);
+static m_com_sized_data_t *realloc_impl(m_context_id_t context, m_com_sized_data_t *data, size_t size);
+static void free_impl(m_context_id_t context, m_com_sized_data_t *data);
 
 m_allocator_t m_slab_allocator = {
     .create = create_context,
@@ -29,27 +29,27 @@ static m_context_id_t create_context(m_allocator_config_t config)
 
 }
 
-static void destroy_context(m_context_id_t id)
+static void destroy_context(m_context_id_t context)
 {
 
 }
 
-static m_com_sized_data_t *malloc_impl(m_context_id_t id, size_t size)
+static m_com_sized_data_t *malloc_impl(m_context_id_t context, size_t size)
 {
 
 }
 
-static m_com_sized_data_t *calloc_impl(m_context_id_t id, uint32_t number, size_t size)
+static m_com_sized_data_t *calloc_impl(m_context_id_t context, uint32_t number, size_t size)
 {
 
 }
 
-static m_com_sized_data_t *realloc_impl(m_context_id_t id, m_com_sized_data_t *data, size_t size)
+static m_com_sized_data_t *realloc_impl(m_context_id_t context, m_com_sized_data_t *data, size_t size)
 {
 
 }
 
-static void free_impl(m_context_id_t id, m_com_sized_data_t *data)
+static void free_impl(m_context_id_t context, m_com_sized_data_t *data)
 {
 
 }
